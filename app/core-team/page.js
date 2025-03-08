7606338115445d5648d3f5e377a05c528e0ebef0
carlers <siycarlwashington@gmail.com>
2025-03-08 21:12:39 +0800

    made file for core team page

---
+++ app/core-team/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import Footer from "../Footer";
+import Image from "next/image";
+
+const CoreTeamPage = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CoreTeamPage;
