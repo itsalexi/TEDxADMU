9aad2b7cf3830aa795daf026ef80ca174e9055fe
carlers <siycarlwashington@gmail.com>
2025-03-08 17:41:12 +0800

    fixed images

--- app/shop/ProductCard.js
+++ app/shop/ProductCard.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const ProductCard = ({ product }) => {
   const { name, image, price, link } = product;
 
   return (
     <div className="bg-gray-900 rounded-lg overflow-hidden shadow-lg hover:shadow-red-900/30 transition-all duration-300 transform hover:-translate-y-1">
-      <div className="relative h-64 w-full">
-        <Image src={image} alt={name} layout="fill" objectFit="cover" />
+      <div className="mt-6 w-full place-items-center">
+        <div className="relative aspect-square w-9/12">
+          <Image src={image} alt={name} layout="fill" objectFit="cover" />
+        </div>
       </div>
       <div className="p-6">
         <h3 className="text-xl font-semibold mb-3 text-white">{name}</h3>
