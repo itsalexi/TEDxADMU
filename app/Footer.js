ad0a62aa07318b94fbd49e95db8e75ab4bfe7eed
carlers <siycarlwashington@gmail.com>
2025-03-09 23:41:04 +0800

    add social media links to footer. use react icons library for link icons

--- app/Footer.js
+++ app/Footer.js
@@ -1,21 +1,51 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import Image from "next/image";
+import { FaInstagram, FaFacebookF, FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-16">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-8 md:mb-0">
+          {/* Logo and Tagline */}
+          <div className="mb-8 md:mb-0 text-center md:text-left">
             <Link href="/">
               <Image src="/logo-white.png" alt="TEDx" width={250} height={0} />
             </Link>
-            <p className="text-gray-400 mt-2 ml-4">
+            <p className="text-gray-400 mt-2">
               Independently organized TED event
             </p>
           </div>
-          <div>
+
+          {/* Social Media Links */}
+          <div className="flex space-x-6 mb-8 md:mb-0">
+            <div className="text-gray-400">Follow us on: </div>
+            <Link
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="text-gray-400 hover:text-red-500 transition text-2xl" />
+            </Link>
+            <Link
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF className="text-gray-400 hover:text-red-500 transition text-2xl" />
+            </Link>
+            <Link
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaXTwitter className="text-gray-400 hover:text-red-500 transition text-2xl" />
+            </Link>
+          </div>
+
+          {/* Copyright Notice */}
+          <div className="text-center md:text-right">
             <p className="text-gray-400">
               © 2025 TEDxAteneoDeManilaU. All rights reserved.
             </p>
