3d8b524b158b8acd414e06f9223a0e02891245a1
carlers <siycarlwashington@gmail.com>
2025-03-15 20:59:21 +0800

    fixed navbar for mobile layout

--- app/Navbar.js
+++ app/Navbar.js
@@ -9,20 +9,21 @@ export default function Navbar() {
   return (
     <>
       <nav className="top-0 w-full z-50 absolute p-10">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto">
           <div className="flex justify-between h-16 items-center">
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 z-1">
               <Link href="/">
                 <Image
                   src="/logo-white.png"
                   alt="TEDx"
                   width={300}
                   height={0}
+                  className="w-auto h-16 md:h-20"
                 />
               </Link>
             </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-8 text-xl">
+            <div className="hidden lg:block text-base">
+              <div className="flex justify-evenly space-x-6 xl:text-xl">
                 <Link href="/">
                   <span className="text-gray-300 hover:text-red-500 px-3 py-2 rounded-md font-medium cursor-pointer transition duration-300">
                     Home
@@ -50,13 +51,13 @@ export default function Navbar() {
                 </Link>
               </div>
             </div>
-            <div className="md:hidden">
+            <div className="lg:hidden">
               <button
                 className="text-gray-300 hover:text-red-500 inline-flex items-center justify-center p-2 rounded-md transition duration-300"
                 onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
               >
                 <svg
-                  className="h-6 w-6"
+                  className="h-8 w-8"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -77,30 +78,35 @@ export default function Navbar() {
 
       {/* Mobile menu */}
       <div
-        className={`md:hidden fixed inset-0 z-40 bg-black bg-opacity-95 transform ${
+        className={`lg:hidden fixed inset-0 z-50 bg-black bg-opacity-95 transform ${
           mobileMenuOpen ? 'translate-x-0' : 'translate-x-full'
         } transition-transform duration-300 ease-in-out`}
-        style={{ top: '64px' }}
+       
       >
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
           <Link href="/">
-            <span className="text-gray-300 hover:text-red-500 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+            <span onClick={() => setMobileMenuOpen(false)} className="text-gray-300 hover:text-red-500 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
               Home
             </span>
           </Link>
           <Link href="/about">
-            <span className="text-gray-300 hover:text-red-500 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-              About
+            <span onClick={() => setMobileMenuOpen(false)} className="text-gray-300 hover:text-red-500 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+              About TEDx
+            </span>
+          </Link>
+          <Link href="/core-team">
+            <span onClick={() => setMobileMenuOpen(false)} className="text-gray-300 hover:text-red-500 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+              Get to Know the Core Team
             </span>
           </Link>
           <Link href="/shop">
-            <span className="text-gray-300 hover:text-red-500 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+            <span onClick={() => setMobileMenuOpen(false)} className="text-gray-300 hover:text-red-500 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
               Shop
             </span>
           </Link>
-          <Link href="/apply">
-            <span className="bg-red-600 text-white hover:bg-red-700 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-              Apply Now
+          <Link href="/register">
+            <span onClick={() => setMobileMenuOpen(false)} className="bg-red-600 text-white hover:bg-red-700 block px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+              Register Now
             </span>
           </Link>
         </div>
