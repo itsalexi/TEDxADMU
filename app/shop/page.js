b0756577c60d1fa4ccee5dec0d05b6ab7352af87
carlers <siycarlwashington@gmail.com>
2025-03-22 17:58:12 +0800

    cleaned imports

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,34 +1,12 @@
 "use client";
 
 import React from "react";
-import ProductCard from "./ProductCard";
-import Image from "next/image";
 import Link from "next/link";
 import { SmoothScrollHero } from "@/components/smoothScroll";
 import { ShopHero } from "@/components/shopHero";
 
 const ShopPage = () => {
-  const link = "https://google.com"; // link to google forms
-  const products = [
-    {
-      name: "TEDxAteneoDeManila T-Shirt Light",
-      image: "/shirt-light.png",
-      price: 20,
-      link: link,
-    },
-    {
-      name: "TEDxAteneoDeManila T-Shirt Dark",
-      image: "/shirt-dark.png",
-      price: 10,
-      link: link,
-    },
-    {
-      name: "TEDxAteneoDeManila T-Shirt Core Team",
-      image: "/shirt-ct.png",
-      price: 15,
-      link: link,
-    },
-  ];
+  
 
   return (
     <div className="min-h-screen bg-black text-white relative">
