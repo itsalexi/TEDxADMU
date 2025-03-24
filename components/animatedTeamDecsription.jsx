aabb89230f5bc35836398572c0b9c6702d66b84a
carlers <siycarlwashington@gmail.com>
2025-03-24 21:07:17 +0800

    made buttons rounded in home page for consistency

--- components/animatedTeamDecsription.jsx
+++ components/animatedTeamDecsription.jsx
@@ -46,7 +46,7 @@ const AnimatedTeamDescription = () => {
         clarity within complexity.
       </p>
       <Link href="/event-details">
-        <span className="inline-block mt-12 bg-[#eb0028] hover:bg-red-700 text-white text-base sm:text-lg md:text-xl px-5 sm:px-6 py-2 sm:py-3 font-medium cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
+        <span className="inline-block rounded-md mt-12 bg-[#eb0028] hover:bg-red-700 text-white text-base sm:text-lg md:text-xl px-5 sm:px-6 py-2 sm:py-3 font-medium cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
           View Topics
         </span>
       </Link>
