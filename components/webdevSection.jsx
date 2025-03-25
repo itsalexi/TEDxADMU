f6e3583e3049af682bedc2294020e955154b8b08
carlers <siycarlwashington@gmail.com>
2025-03-25 21:16:35 +0800

    fixed z index for webdev section

--- components/webdevSection.jsx
+++ components/webdevSection.jsx
@@ -35,7 +35,7 @@ function WebdevSection() {
       className="p-16 pt-48 bg-background text-white"
     >
       <div className="w-full h-full md:text-4xl lg:text-5xl sm:text-3xl text-2xl flex flex-row items-start justify-start bg-background font-normal overflow-hidden p-16 pt-48">
-        <div className="whitespace-pre-wrap z-50 text-white">
+        <div className="whitespace-pre-wrap z-30 text-white">
           <span>{"Special thanks to our "}</span>
           <Typewriter
             text={["Website Development", "GOAT", "amazing", "wonderful"]}
@@ -52,7 +52,7 @@ function WebdevSection() {
         {developers.map((member, index) => (
           <div
             key={index}
-            className="z-40 bg-gradient-to-b from-black to-gray-900 rounded-lg overflow-hidden border border-gray-800"
+            className="z-30 bg-gradient-to-b from-black to-gray-900 rounded-lg overflow-hidden border border-gray-800"
           >
             <div className="relative h-80 w-full">
               {/* If you don't have actual images yet, this will fallback to placeholder */}
