b09f576ea73b339b74947f7b87e38ebd562858fe
carlers <siycarlwashington@gmail.com>
2025-03-25 19:03:54 +0800

    typewriter comopnents

--- app/core-team/page.js
+++ app/core-team/page.js
@@ -6,6 +6,8 @@ import coreTeamData from "./coreTeamMembers.json";
 import CoreTeamCard from "./CoreTeamCard";
 import ParticlesBackground from "../ParticlesBackground";
 import TeamPhotoSection from "@/components/TeamPhotoSection";
+import { Typewriter } from "@/components/typeWriter";
+import { TextShimmer } from "@/components/textShimmer";
 
 const CoreTeamPage = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -213,6 +215,38 @@ const CoreTeamPage = () => {
           </div>
         </div>
       </div>
+
+      {/* WebDev Section */}
+      <section id="webdev-section">
+        <div>
+          <div className="w-full h-full md:text-4xl lg:text-5xl sm:text-3xl text-2xl flex flex-row items-start justify-start bg-background font-normal overflow-hidden p-16 pt-48">
+            <div className="whitespace-pre-wrap z-50 text-white">
+              <span>{"Special thanks to our "}</span>
+              <Typewriter
+                text={[
+                  "Website Development",
+                  "GOAT",
+                  "hard carry",
+                  "insanely handsome",
+                  "infinite aura",
+                ]}
+                speed={70}
+                className="text-[#eb0028]"
+                waitTime={1500}
+                deleteSpeed={40}
+                cursorChar={"_"}
+              />
+              <span>team</span>
+            </div>
+          </div>
+          <TextShimmer
+            duration={1.2}
+            className="text-xl font-medium [--base-color:theme(colors.blue.600)] [--base-gradient-color:theme(colors.blue.200)] dark:[--base-color:theme(colors.blue.700)] dark:[--base-gradient-color:theme(colors.blue.400)]"
+          >
+            Alexi Roth Canamo
+          </TextShimmer>
+        </div>
+      </section>
     </div>
   );
 };
